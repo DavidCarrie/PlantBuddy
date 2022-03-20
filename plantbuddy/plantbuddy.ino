6c44be93c3110212bb40e5aa86716a674fcef2c3
unknown <wangw95@mcmaster.ca>
2022-03-20 15:23:36 -0400

    intergrated user input. Need to decide if we keep the serial output as is, so that the command guide has to be repeated or if we need the user to input a command for viewing the values so that the command screen wont keep refreshing.Also using extern float xxxx we can modify the variables (that are defined in main code) inside the .h file so that's also a scope thing

--- plantbuddy/plantbuddy.ino
+++ plantbuddy/plantbuddy.ino
@@ -16,6 +16,10 @@ int Pin2 = A1;
 
 float water1 = 0;
 float light1 = 0;
+
+float waterThreshold=500;
+float lightThreshold=550;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) {}/*wait for serial*/
@@ -29,11 +33,19 @@ void setup() {
   delay(500);
 }
 void loop() {
+ delay(1000);
+/*BEGIN WIP modules*/
+userInputPrintGuide();
+userInputInLoopCheck();
+/*END WIP modules*/
+
+
+/*BEGIN original raw material from David  */
   Serial.print("MOISTURE LEVEL: ");
   water1 = analogRead(Pin1);
   Serial.println(water1);
  
-  if(water1>500)
+  if(water1>waterThreshold)
   {
       digitalWrite(OWater1, HIGH);
   }
@@ -48,10 +60,16 @@ void loop() {
   light1 = analogRead(Pin2);
   Serial.println(light1);
   Serial.println(); 
-  if (light1 < 550){
+
+  if (light1 < lightThreshold){
     digitalWrite(OLight1, HIGH);
   }else{
     digitalWrite(OLight1, LOW);
   }
-  delay(1000);
+
+/*DEBUG:SHOW ALL VAR*/
+Serial.print("light threshold: ");
+Serial.println(lightThreshold);Serial.print("water threshold: ");
+Serial.println(waterThreshold);
+
 }
---
+++ plantbuddy/user_input.h
@@ -0,0 +1,71 @@
+extern float waterThreshold;
+extern float lightThreshold;
+
+void userInputPrintGuide()
+{
+      Serial.println("\n\n-----------\nCommands guideline:");
+    //Serial.println("type 'check' to check the current water/light threshold value."); // We need to decide if the status output is by default or if the user has to type command to see the status
+    Serial.println("type 'set water' to change the water threshold value.");
+    Serial.println("type 'set light' to change the light threshold value.");
+    //Serial.println("type 'status' to check the current plant value.");
+    Serial.println();
+  }
+
+
+  void userInputInLoopCheck()
+  {
+     if(Serial.available()){
+String command = Serial.readStringUntil('\n');
+        if(command.equals("set water")){
+            
+  Serial.println("Type a number in 5 seconds!");
+  delay(5000);
+  if(Serial.available()){
+    int new_water_value = Serial.parseInt();
+    waterThreshold = new_water_value;
+    Serial.print("The new default water value is: " );
+    Serial.println(waterThreshold);
+    Serial.println("Successful! You can give next command.");
+    Serial.println();
+  }
+  else{
+    Serial.println("No input received. Please give a new command.");
+    Serial.println();
+  }
+        }
+        else if(command.equals("set light")){
+  Serial.println("Type a number in 5 seconds!");
+  delay(5000);
+  if(Serial.available()){
+    int new_light_value = Serial.parseInt();
+    lightThreshold = new_light_value;
+    Serial.print("The new default light value is: " );
+    Serial.println(lightThreshold);
+    Serial.println("Successful! You can give next command.");
+    Serial.println();
+  }
+  else{
+    Serial.println("No input received. Please give a new command.");
+    Serial.println();
+  }        }
+        else if(command.equals("check")){
+  Serial.print("The current water threshold value is: " );
+  Serial.println(waterThreshold);
+  Serial.println();
+  Serial.print("The current light threshold value is: " );
+  Serial.println(lightThreshold);
+  Serial.println();        }
+        //else if(command.equals("status")){   }//NoNeed Status as already showing status in main code(plantbuddy.ino)
+      else{
+            Serial.println("Invalid command");
+        }
+            }
+    }
+
+
+//Directly copied from the User command . ini into .h file  but I think it ll work
+
+ 
+
+ 
+    
