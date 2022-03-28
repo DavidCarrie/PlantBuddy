0e9222828e1a5620e7824d5e7506167fe80a8e22
Gengyun-Wang <73677146+Gengyun-Wang@users.noreply.github.com>
2022-03-27 21:04:11 -0400

    Update light_control.h

--- plantbuddy/light_control.h
+++ plantbuddy/light_control.h
@@ -1,46 +1,57 @@
-unsigned long time;
-String command;
-unsigned long previousBlinkTime;
-int blinkInterval = 5000;
-int light = 0;
+int OLight1 = 2;
+int Pin2 = A1;
+float light1 = 0;
+float lightThreshold=550;
 
-void setup(){
-  Serial.begin(9600);
+int previousOnTime;
+int previousOffTime;
+
+void setupLightModule(){
+  pinMode(OLight1, OUTPUT);
+  pinMode(Pin2, INPUT);
+  digitalWrite(OLight1, HIGH);
 }
 
-void loop(){
-  unsigned long currentMillis = millis(); // get current time
-  if (light == 1){
-    millisBlink(currentMillis);
-  }
+void lightModule(int currentHour, int currentDay, int currentMinute){
+  //light sensoring
+  Serial.print("Light Level: ");
+  light1 = analogRead(Pin2);
+  Serial.println(light1);
+  Serial.println();
   
-  if(Serial.available()){
-      command = Serial.readStringUntil('\n');
-      if(command.equals("time")){
-          print_time();
-      }
-      else if(command.equals("set light")){
-          set_light();
-      }
-  } 
-  delay(1000);
-}
-
-void set_light(){
-  Serial.println("light turn up");
-  light = 1;
-  previousBlinkTime = millis();
-}
-
-void millisBlink(unsigned long currentTime) { 
-  //check time interval
-  if (currentTime - previousBlinkTime >= blinkInterval) {    //if ture
-    Serial.println("light off");                       //turn off light
-    light = 0;
-    previousBlinkTime = currentTime;  //store new previousBlinkTime    
-    
-             
-  }  else if (currentTime - previousBlinkTime <= 0) {  //if time overflow 
-    previousBlinkTime = currentTime;
+  //assume daytime is in range of [7, 19]
+  if (currentHour >= 7 && currentHour < 19 && light1 < lightThreshold){
+    	light_on(currentHour, currentDay, currentMinute);
   }
+  //assume nighttime is in range of [19, 24) & [0, 7)
+  else if (currentHour >= 0 && currentHour < 7 && light1 >= lightThreshold){
+    	light_off(currentHour, currentDay, currentMinute);
+  }
+  else if (currentHour >= 19 && currentHour < 24 && light1 >= lightThreshold){
+    	light_off(currentHour, currentDay, currentMinute);
+ 	}
 }
+
+void light_on(int currentHour, int currentDay, int currentMinute){
+  digitalWrite(OLight1, HIGH);
+  Serial.print("light turn up at day ");
+  Serial.print(currentDay);
+  Serial.print("   hour ");
+  Serial.print(currentHour);
+  Serial.print("   minute ");
+  Serial.print(currentMinute);
+  Serial.println();
+}
+
+void light_off(int currentHour, int currentDay, int currentMinute){
+  digitalWrite(OLight1, LOW);
+  Serial.print("light turn off at day ");
+  Serial.print(currentDay);
+  Serial.print("   hour ");
+  Serial.print(currentHour);
+  Serial.print("   minute ");
+  Serial.print(currentMinute);
+  Serial.println();
+}
+
+
