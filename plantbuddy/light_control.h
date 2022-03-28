08dcf687c16ac511276968249bc52ca6e2f47470
Gengyun-Wang <73677146+Gengyun-Wang@users.noreply.github.com>
2022-03-27 21:27:30 -0400

    Update light_control.h

--- plantbuddy/light_control.h
+++ plantbuddy/light_control.h
@@ -1,5 +1,6 @@
 int OLight1 = 2;
 int Pin2 = A1;
+int light = 0; //represents light status: 0 -> off, 1 -> on
 float light1 = 0;
 float lightThreshold=550;
 
@@ -20,20 +21,24 @@ void lightModule(int currentHour, int currentDay, int currentMinute){
   Serial.println();
   
   //assume daytime is in range of [7, 19]
-  if (currentHour >= 7 && currentHour < 19 && light1 < lightThreshold){
+  if (currentHour >= 7 && currentHour < 19 && light1 < lightThreshold && light == 0){
     	light_on(currentHour, currentDay, currentMinute);
   }
+  else if (currentHour >= 7 && currentHour < 19 && light1 >= lightThreshold && light == 1){
+      light_off(currentHour, currentDay, currentMinute);
+  }
   //assume nighttime is in range of [19, 24) & [0, 7)
-  else if (currentHour >= 0 && currentHour < 7 && light1 >= lightThreshold){
+  else if (currentHour >= 0 && currentHour < 7 && light == 1){
     	light_off(currentHour, currentDay, currentMinute);
   }
-  else if (currentHour >= 19 && currentHour < 24 && light1 >= lightThreshold){
+  else if (currentHour >= 19 && currentHour < 24 && light == 1){
     	light_off(currentHour, currentDay, currentMinute);
  	}
 }
 
 void light_on(int currentHour, int currentDay, int currentMinute){
   digitalWrite(OLight1, HIGH);
+  light = 1;
   Serial.print("light turn up at day ");
   Serial.print(currentDay);
   Serial.print("   hour ");
@@ -44,6 +49,7 @@ void light_on(int currentHour, int currentDay, int currentMinute){
 }
 
 void light_off(int currentHour, int currentDay, int currentMinute){
+  light = 0;
   digitalWrite(OLight1, LOW);
   Serial.print("light turn off at day ");
   Serial.print(currentDay);
